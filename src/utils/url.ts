caa1e6b3e6bcc2ea43f9521017a6942db80a747a
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-06-25 21:32:52 +0200

    Make production the default environment

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,6 +1,6 @@
-export const BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'https://codesandbox.io'
-  : 'http://codesandbox.dev';
+export const BASE_URL = process.env.NODE_ENV === 'development'
+  ? 'http://codesandbox.dev'
+  : 'https://codesandbox.io';
 
 export const CREATE_SANDBOX_URL = BASE_URL + '/api/v1/sandboxes';
 export const GET_USER_URL = BASE_URL + '/api/v1/users/current';
